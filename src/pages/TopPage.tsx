11bc33f74ac962d2b2b9598d650c218bfbcc43ee
Shota Iuchi <shotaiuchi.develop@gmail.com>
2021-06-23 21:53:42 +0900

    ページリンク作成

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -1,11 +1,16 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import FugaTopPage from "./fuga/FugaTopPage";
 import HogeTopPage from "./hoge/HogeTopPage";
 
 const TopPage = () => {
+  const _padding = 10;
   return (
     <Router>
-      <div>
+      <div style={{ padding: _padding, backgroundColor: "#ccccff" }}>
+        <Link to="/">Hoge(TOP)</Link>
+        <Link to="/fuga">Fuga</Link>
+      </div>
+      <div style={{ padding: _padding, backgroundColor: "#ccffcc" }}>
         <Route exact path="/" component={HogeTopPage}></Route>
         <Route path="/fuga" component={FugaTopPage}></Route>
       </div>
