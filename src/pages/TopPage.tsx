ca1981931945c2dce8e7beadf6020fe4d50760e6
Shota Iuchi <shotaiuchi.develop@gmail.com>
2021-06-23 22:56:50 +0900

    ant対応

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -1,3 +1,4 @@
+import { Layout } from "antd";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import FugaTopPage from "./fuga/FugaTopPage";
 import HogeTopPage from "./hoge/HogeTopPage";
@@ -6,14 +7,14 @@ const TopPage = () => {
   const _padding = 10;
   return (
     <Router>
-      <div style={{ padding: _padding, backgroundColor: "#ccccff" }}>
+      <Layout style={{ padding: _padding, backgroundColor: "#ccccff" }}>
         <Link to="/">Hoge(TOP)</Link>
         <Link to="/fuga">Fuga</Link>
-      </div>
-      <div style={{ padding: _padding, backgroundColor: "#ccffcc" }}>
+      </Layout>
+      <Layout style={{ padding: _padding, backgroundColor: "#ccffcc" }}>
         <Route exact path="/" component={HogeTopPage}></Route>
         <Route path="/fuga" component={FugaTopPage}></Route>
-      </div>
+      </Layout>
     </Router>
   );
 };
